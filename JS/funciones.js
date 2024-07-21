bccfe209afae01b4990d61d3ff27172445647b16
Ariel Guevara <adguevara7@espe.edu.ec>
2024-07-21 14:20:05 -0500

    implementacion de funcionalidad a galeria en index.html, funcionalidad dentro de funciones.js

---
+++ JS/funciones.js
@@ -0,0 +1,13 @@
+// Galeria de index
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // aqui se define el tamaño de las iamgenes que vayamos a agregar
+})
