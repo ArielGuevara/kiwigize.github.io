6bcdcfed3cf411ac27dfe0682ac7e0cf6a3e7f6b
Ariel Guevara <adguevara7@espe.edu.ec>
2024-07-21 15:49:50 -0500

    funcionalidad en botones de leer mas en galeria de index.html

--- JS/funciones.js
+++ JS/funciones.js
@@ -11,3 +11,20 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1]) // aqui se define el tamaño de las iamgenes que vayamos a agregar
 })
+
+// funcionalidad de botones de galeria
+document.getElementById('leerMas1').onclick = function(){
+    window.location.href = '../recipes.html';
+}
+document.getElementById('leerMas2').onclick = function(){
+    window.location.href = '../recipes.html';
+}
+document.getElementById('leerMas3').onclick = function(){
+    window.location.href = '../recipes.html';
+}
+document.getElementById('leerMas4').onclick = function(){
+    window.location.href = '../recipes.html';
+}
+document.getElementById('leerMas5').onclick = function(){
+    window.location.href = '../recipes.html';
+}
