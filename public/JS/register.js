006a700f5cbd540ef68167862c7694b5942487b2
Ariel Guevara <adguevara7@espe.edu.ec>
2024-08-30 11:48:53 -0500

    cambios a navegacion

--- public/JS/register.js
+++ public/JS/register.js
@@ -97,7 +97,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     alert("Usuario o contraseña incorrectos");
                     
                 }else{
-                    window.location.href = "./index.html";
+                    window.location.href = "./indexLogeado.html";
                 }
             })
             .catch((error)=>console.log(error)); 
